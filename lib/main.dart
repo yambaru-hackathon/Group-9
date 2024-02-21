dee3144be40e14c18bf28502ca3224b294ffbd55
ThinzarAg <thinzaraung1220@gmail.com>
2024-02-21 09:39:35 +0900

    User class edit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
