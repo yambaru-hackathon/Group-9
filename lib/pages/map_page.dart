84403838b05baafa838575fa06a4ef4a65a844bc
renuehara <potetius1290@gmail.com>
2024-02-21 09:49:37 +0900

    navigationbar

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('マップ'),
+      ),
+      body: const Center(
+          child: Text('マップ画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
