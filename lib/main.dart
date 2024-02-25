880ad655ce80085828085bd7fba23bcb78df0c67
renuehara <potetius1290@gmail.com>
2024-02-25 17:43:06 +0900

    切り替え

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ void main() async {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-        useMaterial3: true,
+      useMaterial3: true,
     ),
     home: MyApp(),
   ));
@@ -32,49 +32,51 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var currentIndex = 0;
 
-  static const _screens = [MapPage(), RoomListPage(), AccountScreen(),];
-  
+  static const _screens = [
+    MapPage(),
+    RoomListPage(),
+    AccountScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      body: SafeArea(
-        child: Stack(
-          // Stack ウィジェットでボトムナビゲーションバーをページの上に重ねる
-          children: [
-            _screens[currentIndex],
-            Positioned(
-              // ボトムナビゲーションバーの位置を指定
-              bottom: 20,
-              left: 20,
-              right: 20,
-              child: Container(
-                height: size.width * .155,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 221, 221, 221),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(.15),
-                      blurRadius: 30,
-                      offset: Offset(0, 10),
-                    )
-                  ],
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buildNavItem(Icons.map_rounded, size),
-                    buildNavItem(Icons.chat_bubble_outline, size),
-                    buildNavItem(Icons.account_circle_rounded, size),
-                  ],
-                ),
+      body: Stack(
+        // Stack ウィジェットでボトムナビゲーションバーをページの上に重ねる
+        children: [
+          _screens[currentIndex],
+          Positioned(
+            // ボトムナビゲーションバーの位置を指定
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: Container(
+              height: size.width * .155,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 221, 221, 221),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(.15),
+                    blurRadius: 30,
+                    offset: Offset(0, 10),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildNavItem(Icons.map_rounded, size),
+                  buildNavItem(Icons.chat_bubble_outline, size),
+                  buildNavItem(Icons.account_circle_rounded, size),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
